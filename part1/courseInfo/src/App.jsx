5ba08002a96174ecedaf91799fcf8381509aed49
Federico Dev <caceresfederico@hotmail.com>
2024-12-31 15:17:15 -0300

    Part 1 - Exercise 1.3 - 1.5 Completed!

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,72 +1,73 @@
 function App() {
-  // APP DATA
-  const name = "Half Stack aplication development"
-  const currentStudents = 250
-  // const part1 = "Fundamentals of React"
-  // const exercises1 = 10
-  // const part2 = "Using props to pass data"
-  // const exercises2 = 7
-  // const part3 = "State of component"
-  // const exercises3 = 14
-
-  // RESTRUCTURED DATA ON ARRAY OF OBJECTS
-  const parts = [
+  // MAIN APP DATA
+  // 1.5 CREATE ONE OBJECT
+  const course = {
+    name: "Half Stack aplication development",
+    currentStudents: 250,
+    // 1.3
+    // const part1 = {
+    //   name: 'Fundamentals of React',
+    //   exercises: 10
+    // }
+    // const part2 = {
+    //   name: 'Using props to pass data',
+    //   exercises: 7
+    // }
+    // const part3 = {
+    //   name: 'State of a component',
+    //   exercises: 14
+    // }
+    // RESTRUCTURED DATA ON ARRAY OF OBJECTS
     // REMEMBER USE NOTATION parts[0].name 
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 }
-  ]
+    // 1.4 ➕ REDUCED 
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 }
+    ]
+  }
+
 
   // RENDERING
   return (
     <>
-      <Header name={name} students={currentStudents} />
-      {/* 1.1 */}
-      {/* <Content
-        part1={part1} part2={part2} part3={part3} exercises1={exercises1}
-        exercises2={exercises2} exercises3={exercises3} /> */}
-      {/* 1.2 */}
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} students={course.currentStudents} />
+      {/* 1.4  💿 THERE IS A BETTER WAY TO REDUCE THIS?*/}
+      <Content name={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Content name={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Content name={course.parts[2].name} exercises={course.parts[2].exercises} />
+      <Total parts={course.parts} />
     </>
   )
-}
 
-// COMPONENTS
-// ADDING DESTRUCTURING SYNTAX
-function Header({ name, students }) {
-  return (
-    <>
-      <h1>Welcome {name}</h1>
-      <h2>Current students {students}</h2>
-    </>
-  )
+  // COMPONENTS
+  // 📀 DESTRUCTURING SYNTAX 
+  function Header({ name, students }) {
+    return (
+      <>
+        <h1>Welcome {name}</h1>
+        <h2>Current students {students}</h2>
+      </>
+    )
+  }
+  function Content({ name, exercises }) {
+    return (
+      <>
+        <p>{name} {exercises}</p>
+      </>
+    )
+  }
+  function Part({ name, exercises }) {
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    )
+  }
+  function Total({ parts }) {
+    // console.log(parts)
+    return (
+      <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>)
+  }
 }
-function Content({ parts }) {
-  // console.log(parts)
-  return (
-    <>
-      {/* 1.1 */}
-      {/* <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p> */}
-      {/* 1.2 */}
-      <Part name={parts[0].name} exercise={parts[0].exercises} />
-      <Part name={parts[1].name} exercise={parts[1].exercises} />
-      <Part name={parts[2].name} exercise={parts[2].exercises} />
-    </>
-  )
-}
-function Part({ name, exercises }) {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-function Total({ parts }) {
-  return (
-    <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>)
-}
-
 export default App
